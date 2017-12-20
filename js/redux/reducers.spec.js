9f0c59d9263e196485ff28dded2e524b621db229
ddaza <danielito@gmail.com>
2017-12-20 02:21:09 -0600

    fixed tests

--- js/redux/reducers.spec.js
+++ js/redux/reducers.spec.js
@@ -1,14 +1,22 @@
 import reducers from './reducers'
 
 test('@@INIT', () => {
-  let state
-  state = reducers(undefined, {})
-  expect(state).toEqual({cartItem: '', imageUrl: ''})
+  const state = reducers(undefined, {})
+  expect(state).toEqual({cartItems: 0, imageUrl: ''})
 })
 
 
 test('SET_IMAGE_URL', () => {
-  let state
-  state = reducers({cartItem: '', imageUrl: ''}, {type: 'SET_IMAGE_URL', imageUrl: 'http://randomimage.com'})
-  expect(state).toEqual({imageUrl: 'http://randomimage.com', cartItem: ''})
+  const state = reducers(undefined, {type: 'SET_IMAGE_URL', imageUrl: 'http://randomimage.com'})
+  expect(state).toEqual({cartItems: 0, imageUrl: 'http://randomimage.com'})
+})
+
+test('SET_CART_ITEM plus', () => {
+  const state = reducers({cartItems: 1, imageUrl: ''}, {type: 'SET_CART_ITEM', cartAction: '+'})
+  expect(state).toEqual({cartItems: 2, imageUrl: ''})
+})
+
+test('SET_CART_ITEM minus', () => {
+  const state = reducers({cartItems: 2, imageUrl: ''}, {type: 'SET_CART_ITEM', cartAction: '-'})
+  expect(state).toEqual({cartItems: 1, imageUrl: ''})
 })
